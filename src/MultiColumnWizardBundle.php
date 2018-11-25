cded42ea399665ca7e6ac15ce14b2549dba43763
Stefan Heimes <stefan_heimes@hotmail.com>
2018-11-25 22:42:16 +0100

    Remove unused elements.

--- src/MultiColumnWizardBundle.php
+++ src/MultiColumnWizardBundle.php
@@ -2,17 +2,8 @@
 
 namespace MenAtWork\MultiColumnWizardBundle;
 
-use Contao\CoreBundle\DependencyInjection\Compiler\AddImagineClassPass;
-use Contao\CoreBundle\DependencyInjection\Compiler\AddPackagesPass;
-use Contao\CoreBundle\DependencyInjection\Compiler\AddResourcesPathsPass;
-use Contao\CoreBundle\DependencyInjection\Compiler\AddSessionBagsPass;
-use Contao\CoreBundle\DependencyInjection\Compiler\DoctrineMigrationsPass;
-use Contao\CoreBundle\DependencyInjection\Compiler\PickerProviderPass;
-use Contao\CoreBundle\DependencyInjection\ContaoCoreExtension;
-use MenAtWork\ClipboardBundle\DependencyInjection\ClipboardExtension;
 use MenAtWork\MultiColumnWizardBundle\DependencyInjection\MultiColumnWizardExtension;
 use Symfony\Component\Console\Application;
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
