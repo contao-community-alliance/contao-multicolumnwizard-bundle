ed32ae10c9878d4902c273b14d13d3e4b27a9803
Stefan Heimes <stefan_heimes@hotmail.com>
2018-11-29 22:29:33 +0100

    Add php-doc header

--- src/Event/GetTinyMceEvent.php
+++ src/Event/GetTinyMceEvent.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * This file is part of MultiColumnWizard.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MultiColumnWizard
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @copyright  Andreas Schempp 2011
+ * @copyright  certo web & design GmbH 2011
+ * @copyright  MEN AT WORK 2013
+ * @license    LGPL
+ */
+
 namespace MenAtWork\MultiColumnWizardBundle\Event;
 
 use Contao\DataContainer;
