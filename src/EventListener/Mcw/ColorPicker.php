3da589d2276b3603c6c706923805bf1cb2b51445
Stefan Heimes <stefan_heimes@hotmail.com>
2019-02-10 15:41:57 +0100

    First version and try for unit test

--- src/EventListener/Mcw/ColorPicker.php
+++ src/EventListener/Mcw/ColorPicker.php
@@ -22,8 +22,11 @@
 
 namespace MenAtWork\MultiColumnWizardBundle\EventListener\Mcw;
 
+use Contao\CoreBundle\Framework\Adapter;
 use Contao\Image;
+use Contao\StringUtil;
 use MenAtWork\MultiColumnWizardBundle\Event\GetColorPickerStringEvent;
+use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  * Class ColorPicker
@@ -31,14 +34,45 @@ use MenAtWork\MultiColumnWizardBundle\Event\GetColorPickerStringEvent;
 class ColorPicker
 {
     /**
+     * Adapter to the image class.
+     *
+     * @var Image|Adapter
+     */
+    private $imageAdapter;
+
+    /**
+     * Adapter to the StringUtil class.
+     *
+     * @var StringUtil|Adapter
+     */
+    private $stringUtilAdapter;
+
+    /**
+     * The translator.
+     *
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * ColorPicker constructor.
+     *
+     * @param Adapter|Image      $imageAdapter      Adapter to the image class.
+     * @param Adapter|StringUtil $stringUtilAdapter Adapter to the StringUtil class.
+     */
+    public function __construct($imageAdapter, $stringUtilAdapter, $translator)
+    {
+        $this->imageAdapter      = $imageAdapter;
+        $this->stringUtilAdapter = $stringUtilAdapter;
+        $this->translator        = $translator;
+    }
+
+    /**
      * Generate the TinyMce Script.
      *
      * @param GetColorPickerStringEvent $event The event.
      *
      * @return void
-     *
-     * @SuppressWarnings(PHPMD.Superglobals)
-     * @SuppressWarnings(PHPMD.CamelCaseVariableName)
      */
     public function executeEvent(GetColorPickerStringEvent $event)
     {
@@ -47,47 +81,39 @@ class ColorPicker
         $fieldId            = $event->getFieldId();
 
         // Support single fields as well (see #5240)
-        $fieldId = $fieldConfiguration['eval']['multiple'] ? $fieldId . '_0' : $fieldId;
+        $fieldId = isset($fieldConfiguration['eval']['multiple']) ? $fieldId . '_0' : $fieldId;
 
         // Crate the placeholder string.
         $placeHolder = <<<HTML
- %s
+ %1\$s
 <script>
   window.addEvent("domready", function() {
-    var cl = $("ctrl_%s").value.hexToRgb(true) || [255, 0, 0];
-    new MooRainbow("moo_%s", {
-      id: "ctrl_%s",
+    var cl = $("ctrl_%2\$s").value.hexToRgb(true) || [255, 0, 0];
+    new MooRainbow("moo_%2\$s", {
+      id: "ctrl_%2\$s",
       startColor: cl,
       imgPath: "assets/colorpicker/images/",
       onComplete: function(color) {
-        $("ctrl_%s").value = color.hex.replace("#", "");
+        $("ctrl_%2\$s").value = color.hex.replace("#", "");
       }
     });
   });
 </script>
 HTML;
 
+        $altText = $this->translator->trans('MSC.colorpicker', [], 'contao_default');
         // Create the image.
-        $imageString = Image::getHtml(
+        $imageString = $this->imageAdapter->getHtml(
             'pickcolor.svg',
-            $GLOBALS['TL_LANG']['MSC']['colorpicker'],
+            $altText,
             sprintf(
                 'title="%s" id="moo_%s" style="cursor:pointer"',
-                \StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['colorpicker']),
+                $this->stringUtilAdapter->specialchars($altText),
                 $fieldId
             )
         );
 
         // Fill all with live.
-        $string = sprintf(
-            $placeHolder,
-            $imageString,
-            $fieldId,
-            $fieldId,
-            $fieldId,
-            $fieldId
-        );
-
-        $event->setColorPicker($string);
+        $event->setColorPicker(sprintf($placeHolder, $imageString, $fieldId));
     }
 }
