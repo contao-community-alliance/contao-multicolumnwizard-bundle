9d96f40dcdbbfbc2099b0fc735ca049d7b3f7a4a
Stefan Heimes <stefan_heimes@hotmail.com>
2019-03-02 23:55:00 +0100

    Some small changes for the autoload.

--- src/deprecated-autoload.php
+++ src/deprecated-autoload.php
@@ -1,20 +1,21 @@
 <?php
 
 /**
- * This file is part of MetaModels/attribute_alias.
+ * This file is part of menatwork/contao-multicolumnwizard-bundle.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2019 MEN AT WORK.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage AttributeAlias
- * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2012-2017 The MetaModels team.
- * @license    https://github.com/MetaModels/attribute_text/blob/master/LICENSE LGPL-3.0
+ * @package    menatwork/contao-multicolumnwizard-bundle
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @copyright  2011 Andreas Schempp
+ * @copyright  2011 certo web & design GmbH
+ * @copyright  2013-2019 MEN AT WORK
+ * @license    https://github.com/menatwork/contao-multicolumnwizard-bundle/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
