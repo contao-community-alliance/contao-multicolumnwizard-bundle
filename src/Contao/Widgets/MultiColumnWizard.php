a70f1fb96bd13b2c254964fe3a2f0a83e3ea85dd
fritzmg <fmg@inspiredminds.at>
2021-02-14 17:43:22 +0000

    fix hidden headers in table view

--- src/Contao/Widgets/MultiColumnWizard.php
+++ src/Contao/Widgets/MultiColumnWizard.php
@@ -1328,41 +1328,41 @@ class MultiColumnWizard extends Widget
         $return = '';
 
         if ($onlyRows == false) {
-            // Generate header fields.
-            foreach ($this->columnFields as $strKey => $arrField) {
-                if ($arrField['eval']['columnPos']) {
-                    $arrHeaderItems[$arrField['eval']['columnPos']] = '<th></th>';
-                } else {
-                    $strHeaderItem = (key_exists($strKey, $arrHiddenHeader)) ? '<th class="hidden">' : '<th>';
+            // Generate header fields if not all are hidden.
+            if (count($this->columnFields) !== count($arrHiddenHeader)) {
+                foreach ($this->columnFields as $strKey => $arrField) {
+                    if ($arrField['eval']['columnPos']) {
+                        $arrHeaderItems[$arrField['eval']['columnPos']] = '<th></th>';
+                    } else {
+                        $strHeaderItem = '<th>' . (key_exists($strKey, $arrHiddenHeader) ? '<div class="hidden">' : '');
+                        if ($arrField['eval']['mandatory']) {
+                            $strHeaderItem .= '<span class="invisible">'
+                            . $GLOBALS['TL_LANG']['MSC']['mandatory']
+                            . ' </span>';
+                        }
+                        $strHeaderItem .=
+                        (
+                            (is_array($arrField['label']))
+                                ? $arrField['label'][0]
+                                : (
+                                    ($arrField['label'] != null)
+                                        ? $arrField['label']
+                                        : $strKey
+                                )
+                        );
+                        if ($arrField['eval']['mandatory']) {
+                            $strHeaderItem .= '<span class="mandatory">*</span>';
+                        }
+                        $strHeaderItem .=
+                        (
+                            (is_array($arrField['label']) && $arrField['label'][1] != '')
+                                ? '<span title="' . $arrField['label'][1] . '"><sup>(?)</sup></span>'
+                                : ''
+                        );
+                        $strHeaderItem .= (key_exists($strKey, $arrHiddenHeader)) ? '</div>' : '';
 
-                    $strHeaderItem .= (key_exists($strKey, $arrHiddenHeader)) ? '<div class="hidden">' : '';
-                    if ($arrField['eval']['mandatory']) {
-                        $strHeaderItem .= '<span class="invisible">'
-                        . $GLOBALS['TL_LANG']['MSC']['mandatory']
-                        . ' </span>';
+                        $arrHeaderItems[] = $strHeaderItem . '</th>';
                     }
-                    $strHeaderItem .=
-                    (
-                        (is_array($arrField['label']))
-                            ? $arrField['label'][0]
-                            : (
-                                ($arrField['label'] != null)
-                                    ? $arrField['label']
-                                    : $strKey
-                              )
-                    );
-                    if ($arrField['eval']['mandatory']) {
-                        $strHeaderItem .= '<span class="mandatory">*</span>';
-                    }
-                    $strHeaderItem .=
-                    (
-                        (is_array($arrField['label']) && $arrField['label'][1] != '')
-                            ? '<span title="' . $arrField['label'][1] . '"><sup>(?)</sup></span>'
-                            : ''
-                    );
-                    $strHeaderItem .= (key_exists($strKey, $arrHiddenHeader)) ? '</div>' : '';
-
-                    $arrHeaderItems[] = $strHeaderItem . '</th>';
                 }
             }
 
